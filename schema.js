2678eec992d1f9a3d432723de2c865e48105c7c0
darrenmwong <darrenwong06@gmail.com>
2014-09-08 15:10:10 -0700

    added save and comparing passwords

--- schema.js
+++ schema.js
@@ -5,7 +5,7 @@ var moongoose = require('moongoose'),
 
 var User = moongoose.model('User', {
   //define model
-  username: {type: String, require: true, index: {unqiue: true} },
+  username: {type: String, require: true, index: {unique: true} },
   password: {type: String, require: true}
 });
 
@@ -15,3 +15,38 @@ var Reservation = moongoose.model('Reservation' {
   //define model
 });
 
+
+//Save
+
+UserSchema.pre(save, function(next {
+  var user = this;
+
+  // Hash password if user doesn't exist or modified
+  if(!user.isModified('password')) return next();
+
+  //generate salt
+  bcrpyt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    //hash the password using our new salt
+    bcrpyt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      //override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+
+});
+
+//Comparing password
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrpyt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = moongoose.model(User&, UserSchema);
